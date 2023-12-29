2fd59546ed68e8449ff523645a0af49174bb740b
Emerida <pedrosoemerida1@gmail.com>
2023-12-29 16:24:57 -0600

    adds .redults.json

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+console.log(cats);
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+console.log(cats);
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+console.log(cats);
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+console.log(cats);
+function appendCat(name) {
+    let newCats = [...cats, name]
+    return newCats;
+}
+let updatedCats = appendCat("Broom");
+console.log(updatedCats);
+console.log(cats);
+function prependCat(name) {
+    let newCats = [name,...cats];
+    return newCats;
+}
+let newCats = prependCat("Arnold");
+console.log(updatedCats);
+console.log(cats);
+function removeLastCat() {
+    let newCats = cats.slice(0, -1);
+    return newCats
+}
+let modifiedCats = removeLastCat();
+console.log(updatedCats);
+console.log(cats);
+function removeFirstCat() {
+    let newCats = cats.slice(1)
+    return newCats
+}
+let catsWithoutFirst = removeFirstCat();
+console.log(updatedCats);
+console.log(cats);
